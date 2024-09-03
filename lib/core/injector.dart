84e91f1569695664e61ab00e76e30883f38d1c7c
Chairil13 <mrnightmare13081999@gmail.com>
2024-09-04 04:35:48 +0700

    update injector dari stateless ke stateful widge

--- lib/core/injector.dart
+++ lib/core/injector.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class Injector extends StatelessWidget {
+class Injector extends StatefulWidget {
   const Injector({super.key, required this.child});
   final Widget child;
 
   @override
+  // ignore: library_private_types_in_public_api
+  _InjectorState createState() => _InjectorState();
+}
+
+class _InjectorState extends State<Injector> {
+  @override
   Widget build(BuildContext context) {
-    return ProviderScope(child: child);
+    return ProviderScope(child: widget.child);
   }
 }
